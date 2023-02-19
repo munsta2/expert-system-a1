987e8b1e30d7f58ff5a1070e9826c6a9ece96fcd
munsta2 <Jessekarwaski@hotmail.com>
2023-02-18 22:46:51 -0500

    added more question to ask the user

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ def main():
         [sg.Button("Enter")]
     ]
 
-    layout = [[sg.Column(into_window(), key='-COL0-'), sg.Column(year_input_window(), visible=False, key='-COL1-'), sg.Column(fav_prof_window(), visible=False, key='-COL2-'), sg.Column(last_layout, visible=False, key='-COL3-')],
-          [sg.Button("Back"),sg.Button('Next'), sg.Button('Exit')]]
+    layout = [
+        
+        [
+        sg.Column(into_window(), key='-COL0-'),
+        sg.Column(year_input_window(),visible=False, key='-COL1-'), 
+        sg.Column(fav_prof_window(), visible=False, key='-COL2-'), 
+        sg.Column(difculty_window(),visible=False,key='-COL3-'),
+        sg.Column(workload_window(),visible=False,key='-COL4-'),
+        sg.Column(last_layout, visible=False, key='-COL5-'),],
+        [sg.Button("Back"),sg.Button('Next'), sg.Button('Exit')]
+    ]
     window = sg.Window('ES for selecting Computer science course', layout,finalize=True,element_justification='c')
     window.TKroot.minsize(200,100)
     window['Back'].update(disabled=True)
@@ -32,6 +41,8 @@ def main():
             layout-=1
             window[f'-COL{layout}-'].update(visible=True)
         elif event in "Enter":
+
+            print(values)
             knowledge_base,explanation = infrence_engine(knowledge_base,values)
 
 
@@ -44,7 +55,7 @@ def main():
         else:
             window['Back'].update(disabled=False)
 
-        if layout >= 3:
+        if layout >= 5:
             window['Next'].update(disabled=True)
         else:
             window['Next'].update(disabled=False)
@@ -60,8 +71,20 @@ def year_input_window():
 
     return text + radio_buttons
 
+def difculty_window():
+    difculty = ["easy","challenging","difficult"]
+    text = [[sg.Text("What level of difficulty do you enjoy in your courses?")]]
+    radio_buttons = [[sg.Radio(x,3,key=x,default=1) for x in difculty]]
+    return text + radio_buttons
+
+def workload_window():
+    workload = ["low","medium","high"]
+    text = [[sg.Text("What level of workload do you enjoy in your courses?")]]
+    radio_buttons = [[sg.Radio(x,4,key=x,default=1) for x in workload]]
+    return text + radio_buttons
+
 def fav_prof_window():
-    prof_list = ["Amr","Passi","Chris","Mark","Gerwal","Czapor"]
+    prof_list = ["Amr","Passi","Mangiardi","Mark","Gerwal","Czapor","Armstrong","Armstrong","Koczkodaj","Mayer"]
 
     layout = [[sg.Text('Who is your favourite Professor?')],
            *[[sg.Radio(x,2,key=x, default=2) for x in prof_list]]
@@ -78,26 +101,31 @@ def infrence_engine(knowledge_base,values):
         if v != target_key:
             temp_dict[k] = v
     values = list(temp_dict.keys())
-    search_list = ["year","professor"]
-
+    print(values)
+    search_list = ["year","professor","difficulty","workload"]
+    used_values = []
 
     for i in range(len(search_list)):
-
+        print("ahhhhhh", i)
         temp_df = knowledge_base.query("{} == '{}'".format(search_list[i],values[i]))
-        
+        print(temp_df)
         if temp_df.empty:
-            pass
+            used_values.append(None)
         else:
+            used_values.append(values[i])
             knowledge_base = temp_df
-    return knowledge_base['course name'].tolist(), explanation_engine(values)
+
+        # print(knowledge_base)
+    return knowledge_base['course name'].tolist(), explanation_engine(used_values)
 
 def explanation_engine(values):
-
+    print(values)
     intro = "We choose this class for you for the following reasons: \n"
     year = "Your currently in {} year \n".format(values[0])
-    prof = "Your favourite profssor is {} \n".format(values[1])
-
-    return intro + year + prof
+    prof = "Your favourite profssor is {} \n".format(values[1]) if values[1] != None else ""
+    difficulty = "you enjoy a course that is {}\n".format(values[2]) if values[2] != None else ""
+    workload = "you enjoy a course with a {} amount of workload \n".format(values[3]) if values[3] != None else ""
+    return intro + year + prof + difficulty + workload
 
 
 
