6ad390b7343581b7fe0b71e360545ab235035e81
munsta2 <Jessekarwaski@hotmail.com>
2023-02-19 20:52:16 -0500

    mostly finished still needs testong

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import PySimpleGUI as sg
 
 def main():
 
-    knowledge_base = pd.read_csv("course knowledgebase.csv")
+    knowledge_base = pd.read_csv("./course knowledgebase.csv")
     last_layout = [
         [sg.Text("press Enter to submit questions")],
         [sg.Button("Enter")]
@@ -18,7 +18,9 @@ def main():
         sg.Column(fav_prof_window(), visible=False, key='-COL2-'), 
         sg.Column(difculty_window(),visible=False,key='-COL3-'),
         sg.Column(workload_window(),visible=False,key='-COL4-'),
-        sg.Column(last_layout, visible=False, key='-COL5-'),],
+        sg.Column(course_material_window(),visible=False,key='-COL5-'),
+        sg.Column(course_length_window(),visible=False,key='-COL6-'),
+        sg.Column(last_layout, visible=False, key='-COL7-'),],
         [sg.Button("Back"),sg.Button('Next'), sg.Button('Exit')]
     ]
     window = sg.Window('ES for selecting Computer science course', layout,finalize=True,element_justification='c')
@@ -28,21 +30,18 @@ def main():
     while True:
 
         event, values = window.read()
-        # print(event)
         if event in (None, 'Exit'):
             break
         elif event in 'Next':
             window[f'-COL{layout}-'].update(visible=False)
             layout+=1
             window[f'-COL{layout}-'].update(visible=True)
-            # print(layout)
         elif event in "Back":
             window[f'-COL{layout}-'].update(visible=False)
             layout-=1
             window[f'-COL{layout}-'].update(visible=True)
         elif event in "Enter":
 
-            print(values)
             knowledge_base,explanation = infrence_engine(knowledge_base,values)
 
 
@@ -55,7 +54,7 @@ def main():
         else:
             window['Back'].update(disabled=False)
 
-        if layout >= 5:
+        if layout >= 7:
             window['Next'].update(disabled=True)
         else:
             window['Next'].update(disabled=False)
@@ -83,6 +82,21 @@ def workload_window():
     radio_buttons = [[sg.Radio(x,4,key=x,default=1) for x in workload]]
     return text + radio_buttons
 
+def course_material_window():
+    text = [[sg.Text("what is your prefered style of course material?")]]
+    material = ["theory","application","mix"]
+    radio = [[sg.Radio(x,6,key=x,default=1)] for x in material]
+    return text + radio    
+
+
+
+def course_length_window():
+    text = [[sg.Text("What is your perfered length of lecture length?")]]
+    length = ["1.5 hours","3 hours"]
+    radio = [[sg.Radio(x,5,key=x,default=1)] for x in length]    
+    return text + radio
+
+
 def fav_prof_window():
     prof_list = ["Amr","Passi","Mangiardi","Mark","Gerwal","Czapor","Armstrong","Armstrong","Koczkodaj","Mayer"]
 
@@ -101,31 +115,28 @@ def infrence_engine(knowledge_base,values):
         if v != target_key:
             temp_dict[k] = v
     values = list(temp_dict.keys())
-    print(values)
-    search_list = ["year","professor","difficulty","workload"]
+    search_list = ["year","professor","difficulty","workload","course_material","lecture_length"]
     used_values = []
 
     for i in range(len(search_list)):
-        print("ahhhhhh", i)
         temp_df = knowledge_base.query("{} == '{}'".format(search_list[i],values[i]))
-        print(temp_df)
         if temp_df.empty:
             used_values.append(None)
         else:
             used_values.append(values[i])
             knowledge_base = temp_df
 
-        # print(knowledge_base)
     return knowledge_base['course name'].tolist(), explanation_engine(used_values)
 
 def explanation_engine(values):
-    print(values)
     intro = "We choose this class for you for the following reasons: \n"
     year = "Your currently in {} year \n".format(values[0])
     prof = "Your favourite profssor is {} \n".format(values[1]) if values[1] != None else ""
     difficulty = "you enjoy a course that is {}\n".format(values[2]) if values[2] != None else ""
     workload = "you enjoy a course with a {} amount of workload \n".format(values[3]) if values[3] != None else ""
-    return intro + year + prof + difficulty + workload
+    material = "you prefer a course with a {} focused course material \n".format(values[4]) if values[3] != None else ""
+    length = "you prefer a course that has {} lectures \n".format(values[5]) if values[5] != None else ""
+    return intro + year + prof + difficulty + workload + material + length
 
 
 
